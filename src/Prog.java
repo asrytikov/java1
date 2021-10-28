b88b70d20daf9af1c168000c2e947d0cf54b2020
asrytikov <44636190+asrytikov@users.noreply.github.com>
2021-10-28 10:34:08 +0400

    Lesson 3

--- src/Prog.java
+++ src/Prog.java
@@ -1,4 +1,7 @@
+import java.util.Arrays;
 import java.util.Locale;
+import java.util.Scanner;
+
 
 public class Prog {
 
@@ -125,7 +128,7 @@ public class Prog {
 
         boolean f = strA.equals(strB);
         System.out.println(f);
-*/
+
         String strS = "Hello world Java";
         String[] masStr = strS.split(" ");
         for (int i = 0; i < masStr.length; i++) {
@@ -144,28 +147,114 @@ public class Prog {
             System.out.print(array[i] + " ");
         }
 
-        int[][] arr = new int[15][15];
+        int[][] arr = new int[2][2];
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
+            for (int j = 0; j < arr[0].length; j++) {
                 arr[i][j] = (int) (Math.random()*100);
             }
         }
+        int sum = 0;
         System.out.println();
         System.out.println("-------------2 array--------------------");
         System.out.println(arr.length);
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
+            for (int j = 0; j < arr[0].length; j++) {
                 System.out.print(arr[i][j] + " ");
+                if (arr[i][j] % 2 == 1) {
+                    System.out.print(arr[i][j] + " ");
+                }
             }
             System.out.println();
             System.out.println("---------------------------------");
 
         }
+        System.out.println("Сумма = "+sum);
     // Найти сумму элементов двумерного массива
         // Распечатать нечетные элементы (значение элемента нечетное число) двумерного массива
 
 
+        int[] mas1 ={0,2,3};
+        int[] mas2 = {3,2,0,6};
+        boolean res = Arrays.equals(mas1, mas2);
+        System.out.println(res);
 
+        int[] mas3 = Arrays.copyOfRange(mas1, 0, 2);
+        for (int i = 0; i < mas3.length; i++) {
+            System.out.println(mas3[i]);
+        }
+
+        System.out.println(Arrays.toString(mas3));
+
+        boolean r = Arrays.equals(mas1, mas3);
+        System.out.println(r);
+
+        Arrays.sort(mas2);
+        System.out.println(Arrays.toString(mas2));
+
+        int foundIndex = Arrays.binarySearch(mas2, 5);
+        System.out.println(foundIndex);
+
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите число:");
+        int myInt = input.nextInt();
+        System.out.printf("Вы ввели %d.%n%n",myInt);
+        double myDouble = input.nextDouble();
+        System.out.printf("Вы ввели %.2f.%n%n",myDouble);
+        System.out.print("Введите строку:");
+        input.nextLine();
+        String str = input.nextLine();
+        System.out.printf("Вы ввели \"%s\".%n%n",str);
+
+        int r = (int)(Math.random()*10);
+
+
+        int a = 5;
+        int b = 10;
+
+        //условие ? значение для true : значение для false
+
+        int numb = 3>2 ? 10 : 5;
+        System.out.println(numb);
+
+        if (3>2){
+            numb = 10;
+        }else{
+            numb = 5;
+        }
+
+        Scanner inp = new Scanner(System.in);
+        String name = inp.nextLine().toUpperCase();
+        switch (name){
+            case "ОЛЕГ":
+                System.out.println("Ваше имя Олег");
+                break;
+            case "ИВАН":
+                System.out.println("Ваше имя Иван");
+                break;
+            default:
+                System.out.println("Fail");
+                break;
+        }
+
+        int[] mas = {1,2,3,4,5};
+        for(int item : mas){
+            System.out.println(item);
+        }
+
+        int cont = 5;
+
+        while (cont > 0){
+            System.out.print(cont);
+            cont--;
+        }
+*/
+        int cont2 = 100;
+        do{
+            System.out.print(cont2);
+            cont2++;
+        } while (cont2<0);
 
 
 
